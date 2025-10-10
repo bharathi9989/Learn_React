fa6eb62f8b4c2f3e432300c47d44e49cfb5097f7
bharathi9989 <er.velubharathi.s@gmail.com>
2025-10-10 20:38:12 +0530

    step1

--- Learn_hooks/1.useState/useStateProject/src/App.jsx
+++ Learn_hooks/1.useState/useStateProject/src/App.jsx
@@ -1,20 +1,66 @@
-import React, { useState } from "react";
-import Userprofile from "./components/userProfile.jsx";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = function () {
-    setCount(count + 1);
-  };
+export default function Form() {
+  const [person, setPerson] = useState({
+    name: "Bharathi",
+    artWork: {
+      title: "blueName",
+      city: "trichy",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
+
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value,
+    });
+  }
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artWork: { ...person.artWork, title: e.target.value },
+    });
+  }
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artWork: { ...person.artWork, city: e.target.value },
+    });
+  }
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artWork: { ...person.artWork, image: e.target.value },
+    });
+  }
+
   return (
     <>
-      <div>
-        <h1>You Clicked {count} times</h1>
-        <button onClick={handleClick}>Click me</button>
-      </div>
-      <Userprofile />
+      <label>
+        Name : <input value={person.name} onChange={handleNameChange} />
+      </label>
+      <label>
+        Title :{" "}
+        <input value={person.artWork.title} onChange={handleTitleChange} />{" "}
+      </label>
+      <label>
+        City : <input value={person.artWork.city} onChange={handleCityChange} />{" "}
+      </label>
+      <label>
+        image :{" "}
+        <input value={person.artWork.image} onChange={handleImageChange} />{" "}
+      </label>
+
+      <p>
+        <i>Name : {person.name} </i>
+        <br />
+        <i>{person.artWork.title}</i>
+        {" by"}
+        <span>Located at {person.artWork.city}</span>
+        <h1>{person.name}</h1>
+        <img src={person.artWork.image} alt={person.artWork.title} />
+      </p>
     </>
   );
 }
-
-export default App;
