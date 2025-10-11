594e950ff2833ba5c6f886785d16df7f6f3284e1
bharathi9989 <er.velubharathi.s@gmail.com>
2025-10-11 08:52:10 +0530

    practice

--- Learn_hooks/1.useState/useStateProject/src/App.jsx
+++ Learn_hooks/1.useState/useStateProject/src/App.jsx
@@ -1,66 +1,30 @@
-import { useState } from "react";
+import React, { Children } from "react";
 
-export default function Form() {
-  const [person, setPerson] = useState({
-    name: "Bharathi",
-    artWork: {
-      title: "blueName",
-      city: "trichy",
-      image: "https://i.imgur.com/Sd1AgUOm.jpg",
-    },
-  });
-
-  function handleNameChange(e) {
-    setPerson({
-      ...person,
-      name: e.target.value,
-    });
-  }
-  function handleTitleChange(e) {
-    setPerson({
-      ...person,
-      artWork: { ...person.artWork, title: e.target.value },
-    });
-  }
-  function handleCityChange(e) {
-    setPerson({
-      ...person,
-      artWork: { ...person.artWork, city: e.target.value },
-    });
-  }
-  function handleImageChange(e) {
-    setPerson({
-      ...person,
-      artWork: { ...person.artWork, image: e.target.value },
-    });
-  }
-
+function App() {
   return (
-    <>
-      <label>
-        Name : <input value={person.name} onChange={handleNameChange} />
-      </label>
-      <label>
-        Title :{" "}
-        <input value={person.artWork.title} onChange={handleTitleChange} />{" "}
-      </label>
-      <label>
-        City : <input value={person.artWork.city} onChange={handleCityChange} />{" "}
-      </label>
-      <label>
-        image :{" "}
-        <input value={person.artWork.image} onChange={handleImageChange} />{" "}
-      </label>
-
-      <p>
-        <i>Name : {person.name} </i>
-        <br />
-        <i>{person.artWork.title}</i>
-        {" by"}
-        <span>Located at {person.artWork.city}</span>
-        <h1>{person.name}</h1>
-        <img src={person.artWork.image} alt={person.artWork.title} />
-      </p>
-    </>
+    <div style={{ paddingLeft: 50 }}>
+      <Toolbar
+        onPlayMovie={() => alert("playing")}
+        uploadImage={() => alert("image uploaded")}
+      />
+    </div>
   );
 }
+function Toolbar({ onPlayMovie, uploadImage }) {
+  return (
+    <>
+      <Button onClick={onPlayMovie}>PlayMovie</Button>
+      <Button onClick={uploadImage}>UploadImage</Button>
+    </>
+  );
+
+  function Button({ onClick, children }) {
+    return (
+      <>
+        <button onClick={onClick}>{children}</button>
+      </>
+    );
+  }
+}
+
+export default App;
