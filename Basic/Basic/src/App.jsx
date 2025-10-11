0ba5342b5f69c5401663a1f1f199ecae6d1163ba
bharathi9989 <er.velubharathi.s@gmail.com>
2025-10-11 11:15:30 +0530

    practice

--- Basic/Basic/src/App.jsx
+++ Basic/Basic/src/App.jsx
@@ -1,35 +1,36 @@
-import React, { useState } from "react";
-import { sculptureList } from "./data";
+import React from "react";
 
-function App() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-  const hasNext = index < sculptureList.length - 1;
-  function handleNextClick() {
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
-  }
-
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
+function Button({ onClick, children }) {
   return (
     <>
-      <button onClick={handleNextClick}>Next</button>
-      <h2>
-        <i>{sculpture.name}</i> by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>{showMore ? "Hide" : "Show"}</button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.name} />
+      <button onClick={onClick}>{children}</button>
+    </>
+  );
+}
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`playing ${movieName} movie`);
+  }
+  return (
+    <>
+      <Button onClick={handlePlayClick}>Play "{movieName}" movie</Button>
+    </>
+  );
+}
+
+function UploadImage() {
+  return (
+    <>
+      <Button onClick={() => alert("uploading")}>Upload Image</Button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <PlayButton movieName="Avengers" />
+      <UploadImage />
     </>
   );
 }
