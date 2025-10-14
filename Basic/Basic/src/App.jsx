8c2dbc2e95feb5a4d12ab70f5886cc2d04f0fd54
bharathi9989 <er.velubharathi.s@gmail.com>
2025-10-14 13:46:42 +0530

    practice

--- Basic/Basic/src/App.jsx
+++ Basic/Basic/src/App.jsx
@@ -1,37 +1,289 @@
-import React from "react";
+// // // import React from "react";
 
-function Button({ onClick, children }) {
-  return (
-    <>
-      <button onClick={onClick}>{children}</button>
-    </>
-  );
-}
-function PlayButton({ movieName }) {
-  function handlePlayClick() {
-    alert(`playing ${movieName} movie`);
-  }
-  return (
-    <>
-      <Button onClick={handlePlayClick}>Play "{movieName}" movie</Button>
-    </>
-  );
-}
+// // // function Button({ onClick, children }) {
+// // //   return (
+// // //     <>
+// // //       <button onClick={onClick}>{children}</button>
+// // //     </>
+// // //   );
+// // // }
+// // // // function PlayButton({ movieName }) {
+// // // //   function handlePlayClick() {
+// // // //     alert(`playing ${movieName} movie`);
+// // // //   }
+// // // //   return (
+// // // //     <>
+// // // //       <Button onClick={handlePlayClick}>Play "{movieName}" movie</Button>
+// // // //     </>
+// // // //   );
+// // // // }
 
-function UploadImage() {
-  return (
-    <>
-      <Button onClick={() => alert("uploading")}>Upload Image</Button>
-    </>
-  );
-}
+// // // // function UploadImage() {
+// // // //   return (
+// // // //     <>
+// // // //       <Button onClick={() => alert("uploading")}>Upload Image</Button>
+// // // //     </>
+// // // //   );
+// // // // }
+
+// // // function ToolBar({ onPlayMovie, onUploadImage }) {
+// // //   return (
+// // //     <>
+// // //       <Button onClick={onPlayMovie}>PlayMovie</Button>
+// // //       <Button onClick={onUploadImage}>uploadImage</Button>
+// // //     </>
+// // //   );
+// // // }
+// // // function App() {
+// // //   return (
+// // //     <>
+// // //       <ToolBar
+// // //         onPlayMovie={() => alert("plaiying")}
+// // //         onUploadImage={() => alert("uploading")}
+// // //       />
+// // //     </>
+// // //   );
+// // // }
+
+// // // export default App;
+// // import React from "react";
+
+// // function App() {
+// //   return (
+// //     <div className="container" onClick={() => alert("div as well running")}>
+// //       <button
+// //         onClick={(e) => {
+// //           e.stopPropagation();
+// //           alert("button 1 clicked");
+// //         }}
+// //       >
+// //         Button 1
+// //       </button>
+// //       <button
+// //         onClick={(e) => {
+// //           e.stopPropagation();
+// //           alert("button 2 clicked");
+// //         }}
+// //       >
+// //         Button 2
+// //       </button>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 5);
+//     setTimeout(() => alert(count), 3000);
+//   }
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={handleClick}>count+5</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import React, { useState } from "react";
+
+// function App() {
+//   const [to, setTo] = useState("Alice");
+//   const [message, setMessage] = useState("hello");
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     setTimeout(() => {
+//       alert(`you said ${message} to ${to}`);
+//     }, 3000);
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           To:{""}
+//           <select value={to} onChange={(e) => setTo(e.target.value)}>
+//             <option value="Velu">Velu</option>
+//             <option value="Bharathi">Bharathi</option>
+//           </select>
+//         </label>
+//         <textarea
+//           placeholder="pls type message here"
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//         ></textarea>
+//         <button type="submit">send</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [position, setPosition] = useState({
+//     x: 1000,
+//     y: 100,
+//   });
+
+//   return (
+//     <div
+//       onPointerMove={(e) => {
+//         setPosition({ x: e.clientX, y: e.clientY });
+//       }}
+//       style={{
+//         position: "relative",
+//         width: "100vw",
+//         height: "100vh",
+//       }}
+//     >
+//       <div
+//         style={{
+//           position: "absolute",
+//           backgroundColor: "red",
+//           borderRadius: "50%",
+//           transform: `translate(${position.x}px , ${position.y}px)`,
+//           width: "100px",
+//           height: "100px",
+//         }}
+//       ></div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [person, setPerson] = useState({
+//     firstName: "Velu",
+//     lastName: "bharathi",
+//     email: "bharathi@gmail.com",
+//   });
+
+//   function handleChange(e) {
+//     setPerson({
+//       ...person,
+//       [e.target.name]: e.target.value,
+//     });
+//   }
+
+//   return (
+//     <div>
+//       <label>
+//         FirstName :
+//         <input
+//           type="text"
+//           name="firstName"
+//           value={person.firstName}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <label>
+//         LastName:
+//         <input
+//           type="text"
+//           name="lastName"
+//           value={person.lastName}
+//           onChange={handleChange}
+//         />
+//       </label>
+//       <label>
+//         Email :
+//         <input type="text" name="email" value={person.email} onChange={handleChange} />
+//       </label>
+//       <p>FirstName : {person.firstName}</p>
+//       <br />
+//       <p>LastName : {person.lastName}</p>
+//       <br />
+//       <p>Email : {person.email}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 
 function App() {
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
+
+  function handleChangeName(e) {
+    setPerson({ ...person, name: e.target.value });
+  }
+  function handleChangeTitle(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
+    });
+  }
+
+  function handleChangeCity(e) {
+    setPerson({
+      ...person,
+      artwork: { ...person.artwork, city: e.target.value },
+    });
+  }
+
+  function handleChangeImage(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
+    });
+  }
   return (
-    <>
-      <PlayButton movieName="Avengers" />
-      <UploadImage />
-    </>
+    <div>
+      <label>
+        Name :{" "}
+        <input type="text" value={person.name} onChange={handleChangeName} />
+      </label>
+      <br />
+      <label>
+        Title
+        <input value={person.artwork.title} onChange={handleChangeTitle} />
+      </label>
+      <br />
+      <label>
+        City :
+        <input
+          type="text"
+          value={person.artwork.city}
+          onChange={handleChangeCity}
+        />
+      </label>
+      <br />
+      <label>
+        Image :
+        <input value={person.artwork.image} onChange={handleChangeImage} />
+      </label>
+      <p>
+        Name : {person.name} title : {person.artwork.title} city :
+        {person.artwork.city}
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </div>
   );
 }
 
